73a89a732c8cc227ef9e5b3a10883e9d6778b667
Jaap van de Sant <jaapsant@gmail.com>
2025-11-21 13:42:50 +0100

    Direct link to gigs should work for logged in users

--- src/components/PrivateRouteWrapper.tsx
+++ src/components/PrivateRouteWrapper.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import { PrivateRoute } from './PrivateRoute';
-import { AuthProvider } from '../context/AuthContext';
-import { GigProvider } from '../context/GigContext';
-import { BandProvider } from '../context/BandContext';
-import { EmailVerificationBanner } from '../components/EmailVerificationBanner';
 
 interface PrivateRouteWrapperProps {
     element: React.ReactElement;
@@ -13,14 +9,7 @@ interface PrivateRouteWrapperProps {
 export const PrivateRouteWrapper: React.FC<PrivateRouteWrapperProps> = ({ element, requireVerification }) => {
     return (
         <PrivateRoute requireVerification={requireVerification}>
-            <AuthProvider>
-                <GigProvider>
-                    <BandProvider>
-                        <EmailVerificationBanner /> {/* Conditionally render based on verification status */}
-                        {element}
-                    </BandProvider>
-                </GigProvider>
-            </AuthProvider>
+            {element}
         </PrivateRoute>
     );
 };
